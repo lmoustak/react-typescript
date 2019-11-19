dcdd097bb24719227103f7c6c1fb5ce2094537c4
Laertes Moustakas <lmoustak@gmail.com>
2019-11-19 21:22:44 +0200

    Added docs

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { AgGridReact } from '@ag-grid-community/react';
 import { AllCommunityModules, GridApi, GridReadyEvent } from '@ag-grid-community/all-modules';
 
 import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
 import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
 
-import {useSearch, DataParams} from "./hooks/useSearch";
+import { useSearch, SearchParams } from "./hooks/useSearch";
 
 
 const Posts: React.FC = () => {
@@ -18,56 +17,20 @@ const Posts: React.FC = () => {
     headerName: "Body", field: "body", sortable: true, filter: true
   }];
 
-  const [rowData, setRowData] = useState<Array<any>>([]);
-  const [id, setId] = useState("");
-  const [query, setQuery] = useState<string | DataParams>("");
-
+  const [query, setQuery] = useState<string | SearchParams>("");
   const [data, loading] = useSearch(query);
-
-  const fetchRows = () => {
-    //setLoading(true);
-
-    const url = "https://jsonplaceholder.typicode.com/posts" + (id ? `/${id}` : "");
-    axios.get(url)
-      .then(({ data: postData }) => {
-        let posts: Array<any> = Array.isArray(postData) ? postData : [postData];
-        let userIds: Array<number> = posts.map((post: { userId: number }) => post.userId);
-        userIds = userIds.filter((userId, index, self) => self.indexOf(userId) === index);
-        let users: Array<any> = [];
-
-        let fetchFunc = async (userId: number) => {
-          let { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
-          users.push(data);
-        };
-
-        let promises = userIds.map(userId => fetchFunc(userId));
-        
-        Promise.all(promises).then(() => {
-          posts.forEach(post => post.username = users.find(user => user.id === post.userId).name);
-          setRowData(posts);
-          //setLoading(false);
-         });
-
-      });
-
-  };
-
-  const handleIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let newValue = event.target.value;
-    setId(isNaN(parseInt(newValue)) ? "" : newValue);
-  };
 
   const handleSearchClick = () => setQuery({
     queryUrl: "https://jsonplaceholder.typicode.com/posts",
     transformations: {
-      transformationUrl: "https://jsonplaceholder.typicode.com/users",
-      fromField: "userId",
-      toField: "username",
-      queryResultField: "id"
+      select: "name",
+      as: "username",
+      from: "https://jsonplaceholder.typicode.com/users",
+      join: "id",
+      on: "userId"
     }
   });
 
-  //useEffect(fetchRows, []);
 
   let gridApi: GridApi;
   const onGridReady = (params: GridReadyEvent) => {
@@ -85,7 +48,6 @@ const Posts: React.FC = () => {
         width: '100%'
       }}
     >
-      {/* <input type="text" value={id} onChange={handleIdChange} /> */}
       <button className="btn btn-primary" onClick={handleSearchClick}>Search</button>
       <AgGridReact
         columnDefs={columnDefs}
--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,64 +1,115 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export interface TransformationObject {
-  transformationUrl: string,
-  fromField: string,
-  toField: string,
-  queryResultField: string,
-  transformationQueryParams?: Array<string>
+/**
+ * An interface to transform given data, akin to JOINing tables in SQL.
+ *
+ * This will replace the values of one column of our given data, with the values of a result query.
+ *
+ * All required params have been written to resemble SQL keywords for a JOIN clause.
+ *
+ * @param {string} select The value to retrieve from the result objects
+ * @param {string} as The name of the new field in the given data to store the values in
+ * @param {string} from The URL to query for the transformation
+ * @param {string} join The field of the result objects to test for equality...
+ * @param {string} on with the field of the given data
+ */
+export interface TransformationParams {
+  select: string,
+  as: string,
+  from: string,
+  join: string,
+  on: string,
+  params?: Array<string>  // TODO Implement at a later stage...
 };
 
-export interface DataParams {
+/**
+ * An interface containing parameters for fetching, and - optionally - transforming data
+ *
+ * @param {string} queryUrl The URL to fetch the data from
+ * @param {TransformationParams} [transformations] Optional, may be a single object or an array of objects.
+ * Contains transformation parameters
+ */
+export interface SearchParams {
   queryUrl: string,
-  transformations?: TransformationObject | Array<TransformationObject>
+  transformations?: TransformationParams | Array<TransformationParams>
 };
 
-const useSearch = (props: string | DataParams): [Array<any>, boolean] => {
+/**
+ * A Hook designed to fetch data from a URL, and optionally transform it by joining it with other data queries.
+ *
+ * @param {(string|SearchParams)} props A string containing the query URL to fetch data from, or a SearchParams object
+ *
+ * @retuns The [data, loading] tuple
+ */
+const useSearch = (props: string | SearchParams = ""): [Array<any>, boolean] => {
   const [data, setData] = useState<Array<any>>([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
     let queryUrl = typeof props === "string" ? props : props.queryUrl;
 
     const search = async () => {
-      let { data: _searchData }: { data: Array<any> | any } = await axios.get(queryUrl);
-      let searchData = Array.isArray(_searchData) ? _searchData : [_searchData];
+      try {
+        let { data: _searchData } = await axios.get(queryUrl);
+        let searchData = Array.isArray(_searchData) ? _searchData : [_searchData];
 
-      if (typeof props !== "string" && props.transformations != null) {
-        let transformations = Array.isArray(props.transformations) ? props.transformations : [props.transformations];
+        if (typeof props !== "string" && props.transformations != null) {
+          let transformations = Array.isArray(props.transformations)
+            ? props.transformations
+            : [props.transformations];
 
-        await Promise.all(transformations.map(async transformation => {
-          let {transformationUrl, fromField, toField, queryResultField} = transformation;
-          if (transformationUrl.lastIndexOf("/") !== transformationUrl.length - 1) {
-            transformationUrl += "/";
-          }
-          let distinctValues = searchData.map((item: any) => item[fromField]).filter((item, index, self) => self.indexOf(item) === index);
-          let transformationQueryResults: Array<any> = [];
-          console.log(distinctValues)
+          await Promise.all(transformations.map(async transformation => {
+            let {
+              select: selectField,  // the result object's key to return
+              from: fromUrl,        // the URL to query for the JOIN
+              as: newField,         // the new key to be added to the given data
+              join: matchField,     // the result key whose value matches...
+              on: originalField     // the given key's value
+            } = transformation;
 
-          await Promise.all(distinctValues.map(async value => {
-            let {data: result} = await axios.get(transformationUrl + value);
-            console.log(result);
+            if (fromUrl.lastIndexOf("/") !== fromUrl.length - 1) {
+              fromUrl += "/";
+            }
 
-            transformationQueryResults.push(result);
+            let distinctValues = searchData
+              .map((item: any) => item[originalField])
+              .filter((value, index, self) => value != null && self.indexOf(value) === index);
+
+            let results: Array<any> = [];
+
+            await Promise.all(distinctValues.map(async value => {
+              try {
+                let { data: result } = await axios.get(fromUrl + value)
+                results.push(result);
+              } catch (err) {
+                console.error(err.response.status);
+              }
+            }));
+
+            if (results.length) {
+              searchData.forEach(item =>
+                item[newField] = results.find(result =>
+                  item[originalField] === result[matchField]
+                )[selectField]
+              );
+            }
           }));
-          console.log(transformationQueryResults[0][queryResultField])
+        }
 
-          searchData.forEach(item => item[toField] = transformationQueryResults.find(result => item[fromField] === result[queryResultField]));
-          console.log(searchData[0]);
-        }));
+        setData(searchData);
+
+      } catch (err) {
+        console.error(err.response.status);
+      } finally {
+        setLoading(false);
       }
-      console.log(searchData)
-      setData(searchData);
-      setLoading(false);
+
     };
 
     if (queryUrl) {
+      setLoading(true);
       search();
-    } else {
-      setLoading(false);
     }
 
   }, [props]);
@@ -66,4 +117,4 @@ const useSearch = (props: string | DataParams): [Array<any>, boolean] => {
   return [data, loading];
 };
 
-export {useSearch};
+export { useSearch };
